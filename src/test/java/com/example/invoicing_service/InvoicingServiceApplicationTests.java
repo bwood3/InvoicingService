65472b3aadb778f2fa2897da09f768a3eb717350
bwood3 <bryanwood1207@gmail.com>
2023-03-31 16:20:48 -0400

    Initial commit

---
+++ src/test/java/com/example/invoicing_service/InvoicingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.invoicing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
