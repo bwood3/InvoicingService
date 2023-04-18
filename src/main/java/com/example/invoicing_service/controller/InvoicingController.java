7e23e35f84b0f7e21431c815aa3032537fc5f432
bwood3 <bryanwood1207@gmail.com>
2023-04-18 18:30:23 -0400

    not found 404 error

--- src/main/java/com/example/invoicing_service/controller/InvoicingController.java
+++ src/main/java/com/example/invoicing_service/controller/InvoicingController.java
@@ -3,12 +3,12 @@ package com.example.invoicing_service.controller;
 import com.example.invoicing_service.model.Invoice;
 import com.example.invoicing_service.model.InvoiceItem;
 import com.example.invoicing_service.model.Order;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
-
+import org.springframework.web.server.ResponseStatusException;
+import reactor.core.publisher.Mono;
+import org.springframework.http.HttpStatus;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
@@ -28,10 +28,18 @@ public class InvoicingController {
     public Invoice findByOrderId(@PathVariable int orderId) {
         Order order = orderService.get().uri("/orders/order/{orderId}", orderId)
                 .retrieve()
+                .onStatus(
+                        HttpStatusCode::is4xxClientError,
+                        response -> Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Order with this ID does not exist in the system"))
+                )
                 .bodyToMono(Order.class).block();
 
+        if (order == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Order with this ID does not exist in the system");
+        }
+
         Invoice invoice = new Invoice();
-        invoice.setOrderPlaced(LocalDate.now()); // You may want to adjust the date based on your requirements.
+        invoice.setOrderPlaced(LocalDate.now());
         invoice.setTotal(order.total());
         invoice.setPayment(order.payment());
 
