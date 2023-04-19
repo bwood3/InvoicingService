2f0acecd8c78a28ead6a410de53611309c09a7bb
bwood3 <bryanwood1207@gmail.com>
2023-04-18 21:57:13 -0400

    PUT v0

--- src/main/java/com/example/invoicing_service/repository/InvoiceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.invoicing_service.repository;
-
-import com.example.invoicing_service.model.Invoice;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InvoiceRepository extends JpaRepository<Invoice, Integer> {
-}
